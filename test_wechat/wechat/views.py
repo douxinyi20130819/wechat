8e26850720070f545eac5ffcbbd041b31f8e2ea0
douxinyi20130819 <dou198565@163.com>
2018-06-15 17:53:34 +0800

    test

--- test_wechat/wechat/views.py
+++ test_wechat/wechat/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render,redirect,HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 import wechat
 import hashlib
 
 # Create your views here.
-
+@csrf_exempt
 def wexin(request):
-    WEIXIN_TPKEN = 'dou20130819'
+    WEIXIN_TPKEN = 'douxinyi20130819'
     if request.method == "GET":
         signature = request.GET.get("signature",None)
         timestamp = request.GET.get("timestamp", None)
@@ -15,9 +16,9 @@ def wexin(request):
         if token and timestamp and nonce:
             tmp_list = [token, timestamp, nonce]
             tmp_list.sort()
-            tmp_str = "%s%s%s" %tuple(tmp_list)
-            tmp_str = hashlib.sha1(bytes(tmp_str,encoding='utf-8')).hexdigest()
-            if tmp_str == signature:
+            string = ''.join(tmp_list).encode('utf-8')
+            string = hashlib.sha1(string).hexdigest()
+            if string == signature:
                 return HttpResponse(echostr)
             else:
                 return HttpResponse("error")
