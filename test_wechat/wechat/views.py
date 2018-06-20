e104a43be0b6ba5a19c0c01aa84bfe5678acab81
douxinyi20130819 <dou198565@163.com>
2018-06-20 21:03:32 +0800

    wechatpy

--- test_wechat/wechat/views.py
+++ test_wechat/wechat/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,redirect,HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-import wechat
+from wechatpy import parse_message,create_reply
+from wechatpy.exceptions import InvalidSignatureException
+from wechatpy.utils import check_signature
 import hashlib
 
 # Create your views here.
@@ -13,96 +15,24 @@ def wexin(request):
         nonce = request.GET.get("nonce", None)
         echostr = request.GET.get("echostr", None)
         token = WEIXIN_TPKEN
-        if token and timestamp and nonce:
-            tmp_list = [token, timestamp, nonce]
-            tmp_list.sort()
-            string = ''.join(tmp_list).encode('utf-8')
-            string = hashlib.sha1(string).hexdigest()
-            if string == signature:
-                return HttpResponse(echostr)
-            else:
-                return HttpResponse("error")
-    if request.method == "POST":
-        othercontent = autoreply(request)
-        return HttpResponse(othercontent)
-import xml.etree.ElementTree as ET
-def autoreply(request):
-    try:
-        webData = request.body
-        xmlData = ET.fromstring(webData)
-
-        msg_type = xmlData.find('MsgType').text
-        ToUserName = xmlData.find('ToUserName').text
-        FromUserName = xmlData.find('FromUserName').text
-        CreateTime = xmlData.find('CreateTime').text
-        MsgType = xmlData.find('MsgType').text
-        MsgId = xmlData.find('MsgId').text
-
-        toUser = FromUserName
-        fromUser = ToUserName
-
-        if msg_type == 'text':
-            content = "您好,欢迎来到Python大学习!希望我们可以一起进步!"
-            replyMsg = TextMsg(toUser, fromUser, content)
-            print("成功了!!!!!!!!!!!!!!!!!!!")
-            print(replyMsg)
-            return replyMsg.send()
-
-        elif msg_type == 'image':
-            content = "图片已收到,谢谢"
-            replyMsg = TextMsg(toUser, fromUser, content)
-            return replyMsg.send()
-        elif msg_type == 'voice':
-            content = "语音已收到,谢谢"
-            replyMsg = TextMsg(toUser, fromUser, content)
-            return replyMsg.send()
-        elif msg_type == 'video':
-            content = "视频已收到,谢谢"
-            replyMsg = TextMsg(toUser, fromUser, content)
-            return replyMsg.send()
-        elif msg_type == 'shortvideo':
-            content = "小视频已收到,谢谢"
-            replyMsg = TextMsg(toUser, fromUser, content)
-            return replyMsg.send()
-        elif msg_type == 'location':
-            content = "位置已收到,谢谢"
-            replyMsg = TextMsg(toUser, fromUser, content)
-            return replyMsg.send()
+        try:
+            check_signature(WEIXIN_TPKEN,signature,timestamp,nonce)
+        except InvalidSignatureException:
+            echostr = 'error'
+        return HttpResponse(echostr,content_type="text/plain")
+    elif request.method == "POST":
+        msg = parse_message(request.body)
+        if msg.type == 'text':
+            repr = create_reply('这是条文字消息', msg)
+        elif msg.type == 'image':
+            repr = create_reply('这是条图片消息', msg)
+        elif msg.type == 'voice':
+            repr = create_reply('这是条语音消息', msg)
         else:
-            msg_type == 'link'
-            content = "链接已收到,谢谢"
-            replyMsg = TextMsg(toUser, fromUser, content)
-            return replyMsg.send()
-    except Exception as Argment:
-        return Argment
+            repr = create_reply('这是条其他类型消息', msg)
+        return HttpResponse(repr.render(),content_type='application/xml')
+    else:
+        pass
 
-class Msg(object):
-    def __init__(self, xmlData):
-        self.ToUserName = xmlData.find('ToUserName').text
-        self.FromUserName = xmlData.find('FromUserName').text
-        self.CreateTime = xmlData.find('CreateTime').text
-        self.MsgType = xmlData.find('MsgType').text
-        self.MsgId = xmlData.find('MsgId').text
-
-import time
-class TextMsg(Msg):
-    def __init__(self, toUserName, fromUserName, content):
-        self.__dict = dict()
-        self.__dict['ToUserName'] = toUserName
-        self.__dict['FromUserName'] = fromUserName
-        self.__dict['CreateTime'] = int(time.time())
-        self.__dict['Content'] = content
-
-    def send(self):
-        XmlForm = """
-        <xml>
-        <ToUserName><![CDATA[{ToUserName}]]></ToUserName>
-        <FromUserName><![CDATA[{FromUserName}]]></FromUserName>
-        <CreateTime>{CreateTime}</CreateTime>
-        <MsgType><![CDATA[text]]></MsgType>
-        <Content><![CDATA[{Content}]]></Content>
-        </xml>
-        """
-        return XmlForm.format(**self.__dict)
 
 
